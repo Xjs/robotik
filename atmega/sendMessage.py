a4c90fc4f665d4fd4d1e9889e3c918ac87cf2d51
glevF <feher@stud.uni-heidelberg.de>
2015-02-22 17:14:57 +0100

    Modified to send message via non-designated GPIO pin

---
+++ atmega/sendMessage.py
@@ -0,0 +1,28 @@
+import serial
+import pigpio
+import time
+from array import array
+
+def sendMessage(deg=0.0, speed=0.0):
+	TXD = 23
+	RXD = 24
+
+	pi = pigpio.pi()
+
+	pi.bb_serial_read_open(RXD, 9600, 8) #8 is the bits per word i.e. one byte - alternatively 80 bits == one message?
+
+	pi.set_mode(TXD,pigpio.OUTPUT)
+	pi.wave_clear()
+	
+	prep_message = array('B',[deg,speed]) #create an array of unsigned chars 
+	message = array('B', [255, prep_message, 0]) #create an array started off by 0xFF, followed by the message in machine values and ended with 0x00
+	
+	pi.wave_add_serial(TXD, 9600, message)
+	wid = wave_create()
+
+	while True:
+		try:
+			pi.wave_send_repeat(wid)
+		except Exception, e:
+			time.sleep(1)
+			continue
