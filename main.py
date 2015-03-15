b0e62e8e5b4c7bf3a2e7eb3c6565f8f267bcbe81
Jannis Andrija Schnitzer <jannis@schnitzer.im>
2015-03-16 00:18:26 +0100

    Pseudocode, der quasi die gesamte Programmlogik enthaelt (10 Zeilen) :D

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+	# GPS-Position bekommen
+	# Entweder: (Re-)Initialisierung – geradeausfahren, Orientierung holen (Kompaß können wir glaub ich nicht vertrauen), kreiseln, bis man drauf zuschaut, anfangen, geradeaus zu fahren
+	# Oder: Wir fahren noch
+	# 	immer mal wieder Position updaten
+	# 	mit der Linie vergleichen, und wenn wir zu sehr abweichen, mal wieder von vorn
+	# 	Hindernis? Wenn ja: Ausweich-Subroutine und dann von vorn
+	
+# Ausweich-Subroutine: Lenk solange vom Hindernis weg, bis es nicht mehr da ist
+# Wenn es nicht weggeht oder auf beiden Seiten eines gemessen wird… vielleicht rückwärts fahren?
