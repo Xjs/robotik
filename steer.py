4c0a0bfa1406823b617c0812631af4ddbe87b762
glevF <feher@stud.uni-heidelberg.de>
2015-03-18 13:01:30 +0100

    Updated

--- steer.py
+++ steer.py
@@ -4,15 +4,14 @@
 #curve radius is approximated by f(x) = 0.715 * 1/deg
 #deg is a float between -1.0 and 1.0 which is used by the atmega's steer method
 
-#rad is the curve radius (max. 0.715 for full right, max. -0.715 for full left) 
+#rad is the curve radius (min. 0.715 for full right, min. -0.715 for full left) 
 
 import sendMessage.py
  
 def steer(rad):
-	if (rad < -0.715):
-		return
-	elif (rad > 0.715):
+	if (abs(rad) < 0.715):
 		return
 	else:
 		deg = 0.715/rad
 		sendMessage(deg, 0.2)
+		return
