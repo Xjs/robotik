b6d5bea3e80c999ce72163cca5fe8718a0fec9a0
lordmaulwurf <tv@in-hd.de>
2015-03-18 11:37:18 +0100

    Update getDistance.py
    zweiter Sensor hinzugefügt. distance(sensor) kriegt 0 oder 1 für einen der beiden Sensoren.

--- getDistance.py
+++ getDistance.py
@@ -6,24 +6,48 @@ GPIO.setmode(GPIO.BCM)
 trig = 18
 echo = 24
 
+trig2 = 22
+echo2 = 23
+
 GPIO.setup(trig, GPIO.OUT)
 GPIO.setup(echo, GPIO.IN)
 
-def distance():
-        GPIO.output(trig, True)
-        time.sleep(0.00001)
-        GPIO.output(trig, False)
+GPIO.setup(trig2, GPIO.OUT)
+GPIO.setup(echo2, GPIO.IN)
 
-        startTime = time.time()
-        stopTime = time.time()
 
-        #write startTime
-        while GPIO.input(echo) == 0:
+def distance(sensor)
+
+        if(sensor==0)
+                GPIO.output(trig, True)
+                time.sleep(0.00001)
+                GPIO.output(trig, False)
+
+                startTime = time.time()
+                stopTime = time.time()
+
+                #write startTime
+                while GPIO.input(echo) == 0:
                 startTime = time.time()
 
-        #write time of signal reaching sensor
-        while GPIO.input(echo) == 1:
+                 #write time of signal reaching sensor
+                while GPIO.input(echo) == 1:
+                        stopTime = time.time()
+        else
+                GPIO.output(trig2, True)
+                time.sleep(0.00001)
+                GPIO.output(trig2, False)
+
+                startTime = time.time()
                 stopTime = time.time()
+
+                #write startTime
+                while GPIO.input(echo2) == 0:
+                startTime = time.time()
+
+                 #write time of signal reaching sensor
+                while GPIO.input(echo2) == 1:
+                        stopTime = time.time()
 
         timeElapsed = stopTime - startTime
 
@@ -33,14 +57,5 @@ def distance():
 
         return distance
 
-if __name__ == '__main__':
-        try:
-                while True:
-                        dist = distance()
-                        print("%.1f cm" % dist)
-                        time.sleep(0.5)
-
-        except KeyboardInterrupt:
-                GPIO.cleanup()
 
 
