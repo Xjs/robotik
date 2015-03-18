ba6dd731534ca1b40c20231522fbc697ff135573
glevF <feher@stud.uni-heidelberg.de>
2015-03-18 11:44:33 +0100

    Concept changed

--- drive.py
+++ drive.py
@@ -8,8 +8,11 @@ speed = 1.6
 
 #drives for m meters at speed of 1.6 m/s
 
-def drive(m):
-	s = m/speed
-	sendMessage(0.0, 0.2) #start driving
-	time.sleep(s)		  #drive m meters
-	sendMessage(0.0, 0.0) #stop car
+#def drive(m):
+#	s = m/speed
+#	sendMessage(0.0, 0.2) #start driving
+#	time.sleep(s)		  #drive m meters
+#	sendMessage(0.0, 0.0) #stop car
+
+def drive():
+	sendMessage(0.0, 0.2)
