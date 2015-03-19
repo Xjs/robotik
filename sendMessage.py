2a8a636fdb6152cfc7764d4a8b0a3878bed840c2
glevF <feher@stud.uni-heidelberg.de>
2015-03-19 23:28:31 +0100

    Errors corrected

--- sendMessage.py
+++ sendMessage.py
@@ -1,12 +1,21 @@
 import serial
 import pigpio
 import time
+import signal
+import sys
+from struct import pack
 from array import array
 
+#def handler(signum, frame):
+#	print "Exiting..."
+#	pi.bb_serial_read_close(RXD)
+#	pi.wave_delete(wid)
+#	pi.stop()
+#	sys.exit(0)
 
 def sendMessage(deg=0.0, speed=0.0):
-	TXD = 23
-	RXD = 24
+	TXD = 17
+	RXD = 22
 
 	pi = pigpio.pi()
 
@@ -16,16 +25,25 @@ def sendMessage(deg=0.0, speed=0.0):
 	pi.wave_clear()
 
 	
-	prep_message = array('B',[deg,speed]) #create an array of unsigned chars 
-	message = array('B', [255, prep_message, 0]) #create an array started off by 0xFF, followed by the message in machine values and ended with 0x00
+	#prep_message = array('B',[deg,speed]) #create an array of unsigned chars 
+	#message = array('B', [255, prep_message, 0]) #create an array started off by 0xFF, followed by the message in machine values and ended with 0x00
 
+	prep_message = bytes()
+	num = [255.0, deg,speed, 0.0]
+	prep_message = prep_message.join((pack('f', val) for val in num))
 	
-	pi.wave_add_serial(TXD, 9600, message)
-	wid = wave_create()
+	pi.wave_add_serial(TXD, 9600, prep_message)
+	wid = pi.wave_create()
+
+#	signal.signal(signal.SIGINT,handler)
 
 	while True:
 		try:
-			pi.wave_send_repeat(wid)
+			pi.wave_send_once(wid)
 		except Exception, e:
 			time.sleep(1)
 			continue
+
+
+if __name__ == "__main__":
+	sendMessage()
