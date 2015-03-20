ce95c6833e92efa82c66a8d744e0a6df6f19481e
Jannis Andrija Schnitzer <jannis@schnitzer.im>
2015-03-20 18:50:59 +0100

    Syntaxfehlerchen, die fuenfte

--- sendMessage.py
+++ sendMessage.py
@@ -53,7 +53,7 @@ def sendMessage(deg=0.0, speed=0.0):
 	prep_message = bytes()
 	num = [deg,speed]
 	strings = [pack('f', val) for val in num]
-	checksums = [pack('c', int(''.join(['0b0000', checksum(string)]),2)) for string in strings]
+	checksums = [pack('c', chr(int(''.join(['0b0000', checksum(string)]),2))) for string in strings]
 	prep_message = bytes.join(bytes.join(tuple) for tuple in zip(strings, checksums))
 	for i in range(len(prep_message)):
 		if prep_message[i] == '\xff':
