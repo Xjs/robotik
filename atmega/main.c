317e8349eddb8c7e0b2ad49d8448047d3b9bbea0
Jannis Andrija Schnitzer <jannis@schnitzer.im>
2015-03-23 20:54:30 +0100

    Test der Interrupt-basierten Kommunikation

--- atmega/main.c
+++ atmega/main.c
@@ -3,9 +3,11 @@
  *	2009 Benjamin Reh und Joachim Schleicher
  */
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <inttypes.h>
 #include <util/delay.h>
 #include <stdlib.h>
+#include <stddef.h>
 #include "uart.h"
 #include "adc.h"
 #include "pwm.h"
@@ -14,12 +16,65 @@
 
 void init();
 
+/* Define two buffers for interrupt-based reading UART data
+ * We write into one buffer via interrupts
+ * When we want to read out a buffer, we first switch the current buffer
+ */
+
+#define BUFFER_SIZE 64
+
+unsigned char buffer[2][BUFFER_SIZE];
+unsigned short int buffer_level[2];
+unsigned short int ab; // active buffer 0 or 1
+unsigned short int buffer_status[2]; // 0: nothing special, 1: start byte received, 2: start and end byte received, 3: buffer full
+
+#define BUFFER_NOTHING 0
+#define BUFFER_START 1
+#define BUFFER_END 2
+#define BUFFER_FULL 3
+
+#define START_BYTE '\xff'
+#define END_BYTE '\0'
+
+void switch_buffers(void)
+{
+	ab = ab ? 0 : 1;
+}
+
+void empty_buffer(unsigned int b)
+{
+	buffer_level[b] = 0;
+	buffer_status[b] = BUFFER_NOTHING;
+}
+
+char *find_message(unsigned char *buffer, size_t buffer_size)
+{
+	int i;
+	int found = 0;
+	char *result = NULL;
+	for (i = 0; i < buffer_size && buffer[i]; i++)
+	{
+		if (buffer[i] == START_BYTE)
+		{
+			result = buffer+i;
+		}
+		else if (result && buffer[i] == END_BYTE)
+		{
+			found = 1;
+		}
+	}
+	
+	if (found)
+		return result;
+	else
+		return NULL;
+}
+
 int main(void)
 {
 	//Initialisierung ausfuehren
 	
 	init();
-	
 	
 	// 1x Ground auf dem MC mit Schaltung verbinden reicht aus. 
 	// Test Lenkung ACHTUNG PORTB0 = Servo 0
@@ -34,9 +89,7 @@ int main(void)
 	//_delay_ms(3000);	
 	//setServo(0, BASE_S);
 	//_delay_ms(3000);		
-
-
-
+	
 	// Test Motor ACHTUNG PORTB1 = Servo 1
 	//drive(0.5, 2000);
 	//drive(0, 2000);
@@ -45,13 +98,33 @@ int main(void)
 	//setServo(1, (BASE + (0.9)*FORWARD));
 	//_delay_ms(2000);
 
-	unsigned char *message = (unsigned char *) malloc(sizeof(char)*(STRIPPED_SIZE));
-	if (!message)
-		return -1;
+	unsigned int rb; // read buffer
+	unsigned char *message;
+//	unsigned char *message = (unsigned char *) malloc(sizeof(char)*(STRIPPED_SIZE));
+//	if (!message)
+//		return -1;
 
-	while (1){
-//		_delay_ms(6000000);
+	while (1)
+	{
+		while (buffer_status[ab] != BUFFER_END)
+		{
+			// no message received -- do nothing
+			;
+		}
 		
+		rb = ab;
+		// message received
+		switch_buffers();
+		
+		message = find_message(buffer[rb], BUFFER_SIZE);
+		if (message) {
+			// handeln Sie!
+			uart_puts(":");
+			uart_puts(message);
+			uart_puts(";");
+		}
+//		_delay_ms(6000000);
+/*		
 		uart_gets(message);
 		
 		if(0 && message[0] == (unsigned char)'\xff' && message[STRIPPED_SIZE-1] == (unsigned char) '\x00') {
@@ -89,8 +162,8 @@ int main(void)
 			uart_putm(message);
 			uart_puts(":@");
 		}
-
-	}	
+*/
+	}
 
 	free(message);
 }
@@ -104,4 +177,24 @@ void init()
 	PWMInit();    // Pulsweite auf D6 ausgeben 
 	timerInit();  // "Systemzeit" initialisieren
 	servoInit();  // Servoansteuerung initialisieren
+	sei(); // // Enable the Global Interrupt Enable flag so that interrupts can be processed
+}
+
+ISR(USART_RXC_vect)
+{
+	char received_byte;
+	received_byte = UDR;
+	if (buffer_level[ab] < BUFFER_SIZE)
+	{
+		buffer[ab][buffer_level[ab]++] = received_byte;
+		if ((buffer_status[ab] == BUFFER_NOTHING && received_byte == START_OF_MESSAGE) || 
+			(buffer_status[ab] == BUFFER_START && received_byte == END_OF_MESSAGE))
+		{
+			buffer_status[ab]++;
+		}
+	}
+	else
+	{
+		buffer_status[ab] = BUFFER_FULL;
+	}
 }
