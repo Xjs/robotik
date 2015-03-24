f78d57c02586efea81aa9c730e0e513ed461722a
Jannis Andrija Schnitzer <jannis@schnitzer.im>
2015-03-24 19:44:09 +0100

    my impression of a main loop

--- main.py
+++ main.py
@@ -7,13 +7,18 @@ import sys
 from math import sin, cos, acos, pi
 from obstancle import *
 from drive import *
-from mgps/navigate import *
+from mgps import GPSTracker
+from mgps.navigate import Navigator, TRESHOLD
 
-angularSpeed = 1.6 * (2*pi) #per second
+RADIUS = 0.715
+SPEED = 1.6
+DEFAULT = -0.07
 
-tracker = GPSTracker()
-navi = Navigator(tracker)
-navi.setRadius(0.715)
+def angular_speed(radius):
+	# TODO: this isn't radius-dependent yet
+	# TODO: measure
+	return 1.6 * (2*pi) #per second
+
 #----------------
 #A simple function that takes two GPS coordinates as input and outputs the distance between them in meter. More approaches can be found here
 
@@ -34,8 +39,8 @@ navi.setRadius(0.715)
 #}
 #--------------------
 
-def approxDistance(target):
-	a = tracker.getPosition()
+def approxDistance(current, target):
+	a = current
 	b = target
 	
 	pk = 180/3.14169
@@ -54,57 +59,61 @@ def approxDistance(target):
 	
 	return 6366000*tt
 	
-def correctCourse():
-	(direction, angle, rad),(start,target) = navigate(target)
-	s = angle/angularSpeed
+def is_at(current, target):
+	return approxDistance(current, target) < TRESHOLD
+	
+def correct_course(direction, angle, radius):
+	s = angle/angular_speed(radius)
 	
 	start = time.time()
-	steer(0.715)
+	steer(direction*radius)
 	while ((time.time() - start) < s):
+		# TODO: obstancle()?
 		pass
-	steer(-0.07)
+	steer(DEFAULT)
 
 def mainRoutine(target):
+	tracker = GPSTracker()
+	navigator = Navigator(tracker)
+	navigator.setRadius(RADIUS)
+	speed = SPEED
 	#Hindernis checken
 	obstancle()
 	# GPS-Position bekommen
 	curPos = tracker.getPosition()
 	time.sleep(2)
+	line = None
+	circle = None
 	while True:
-		if curPos == tracker.getPosition():	#car stands still
-			# Entweder: (Re-)Initialisierung – geradeausfahren, Orientierung holen (Kompass koennen wir glaub ich nicht vertrauen), kreiseln, bis man drauf zuschaut, 			  anfangen, geradeaus zu fahren
+		if is_at(curPos, tracker.getPosition()):	# car stands still
+			# Entweder: (Re-)Initialisierung – geradeausfahren, Orientierung holen (Kompass koennen wir glaub ich nicht vertrauen)
 			start = time.time()
-			drive(1.6)			#drive for 5m
-			while (time.time() - start) < 3:	#while driving (ca. 3 s) save positions to tracker
-				stop()
+			drive(speed)			#drive for 5m
+			while abs(time.time() - start) < 3:	#while driving (ca. 3 s) save positions to tracker
+				# TODO: check for obstacles ... all the time! :)
+				obstancle()
 				tracker.getPosition()
-#			orientation = tracker.getOrientation()
-			#check if orientation is correct
-			(direction, angle, rad),(start,target) = navigate(target)
-			if on_track((start,target)) == True:
-				#if so: drive for approximate number of meters until destination
-				drive(1.6)
-				if tracker.getPosition == target: #target reached, stop car
-					stop()
-					break
-			else:
-				correctCourse()
-				
-		else: #car is moving
+			stop()
+		else:
+			driving = True
+		if is_at(tracker.getPosition(), target):
+			stop()
+			# stunt()
+			break
+		# 	mit der Linie vergleichen, und wenn wir zu sehr abweichen, mal wieder von vorn
+		if not on_track(line):
+			# wenn line noch nicht gesetzt ist (1. Mal), landen wir auch hier
+			# jetzt: kreiseln, bis man drauf zuschaut, anfangen, geradeaus zu fahren
+			stop()
+			circle, line = navigator.navigate(target)
+			correct_course(*circle)
+			drive(speed)
+		if driving:
 		# Oder: Wir fahren noch
 		# 	immer mal wieder Position updaten
-			tracker.getPosition()
-		# 	mit der Linie vergleichen, und wenn wir zu sehr abweichen, mal wieder von vorn
-#			orientation = tracker.getOrientation()
-			(direction, angle, rad),(start,target) = navigate(target)
-			if on_track((start,target)) == True:
-				drive(1.6) 
-				if tracker.getPosition == target: #target reached, stop car
-					stop()
-			else:
-				correctCourse()
-				
-		
+			curPos = tracker.getPosition()
+			obstancle()
+#			orientation = tracker.getOrientation()	
 	
 # Ausweich-Subroutine: Lenk solange vom Hindernis weg, bis es nicht mehr da ist
 # Wenn es nicht weggeht oder auf beiden Seiten eines gemessen wird… vielleicht rückwärts fahren?
