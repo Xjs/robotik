529581ec2a62f047bd841b8c65d5f52a9ca65d7f
lordmaulwurf <tv@in-hd.de>
2015-03-24 19:53:42 +0100

    Update obstancle.py
    Everything is new!

--- obstancle.py
+++ obstancle.py
@@ -1,54 +1,98 @@
 from getDistance.py import *
-from steer.py import *
-from stop.py import *
 from drive.py import *
+from math.py import abs
+import time
 
 #to-do: Überlegen wie man aus Sackgassen und ähnlichen Fallen wieder rauskommt (Rückwärts fahren und so)
 #threshold in [m] gibt die Maximaldistanz zu beachtender Objekte aus.
 threshold = 2
-bullshitdist = 6
-runs = 3
+bullshitdist = 4
+tolerance = 0.005
+lowpass = 0.2
+measrange = 6
+movefactor = 2
+averageminimum = 2
+deathreshold = 0.3
 
-#runs ist die Zahl der Messvorgänge-1 über die gemittelt wird um eine Fehlerarme Distanz zu berechnen.
 
+class watch:
 
-# def average(initial,sensor):
-	# dist = initial/runs
+	def __init__(self):
+		#threading.Thread.__init__(self)
+		self.watchlistL = []
+		self.watchlistR = []
 	
-	# #Mittelung über mehrere Messvorgänge. Fällt ein Wert aus dem Rahmen ( a,b > threshold ) wird er ignoriert.
-	# for i in range(runs-1):
-		# s = distance(sensor)
-		# #Wenn ein zu großer Wert gemessen wird, geht der initiale Wert ein weiteres mal in die Messung ein.
-		# if (s < bullshitdist):
-			# dist = dist + s/runs
-		# else:
-			# dist = dist + initial/runs
-	# stop()
-	# return dist
-
-def obstacle():
-	a = distance(a,0);
-	b = distance(b,1);
-	
-	# if((a < threshold) or (b < threshold)):
-		# a = average(0)
-		# b = average(1)
-	
-	#Permanente Überprüfung ob a,b unter der Maximaldistanz anzeigen
-		while((a < threshold) or (b < threshold)):
+	def alarm():
+		
+		averagesL = []
+		
+		for eins, zwei in zip(self.watchlistL, self.watchlistL[1:]):
+			if (abs(eins - zwei) > lowpass and eins < bullshitdist and zwei < bullshitdist)
+				averagesL.append((eins+zwei)/2)
+				
+		averagesR = []
+		
+		for eins, zwei in zip(self.watchlistR, self.watchlistR[1:]):
+			if (abs(eins - zwei) > lowpass and eins < bullshitdist and zwei < bullshitdist)
+				averagesR.append((eins+zwei)/2)
+		
+		alarmL = bullshitdist
+		alarmR = bullshitdist
+		
+		if (averagesL[len(averagesL)-1] < threshold and len(averagesL) > averageminimum)
 			
-		#Links/Rechts sind die gemittelten Werte der Entfernungsmessung der Sensoren. a/b speichern die aktuelle Entfernungsmessung.
+			for eins, zwei in zip(averagesL, averagesL[1:]):
+				if (eins < zwei):
+					alarmL = bullshitdist
+					break
+				else:
+					alarmL = averagesL[len(averagesL)-1]
+		
+		if (averagesR[len(averagesR)-1] < threshold and len(averagesR) > averageminimum)
+			
+			for eins, zwei in zip(averagesR, averagesR[1:]):
+				if (eins < zwei):
+					alarmR = bullshitdist
+					break
+				else:
+					alarmR = averagesR[len(averagesR)-1]
+					
+		return (alarmL, alarmR)
 	
-			if(a<b):
-				steer(0.715)
-			else:
-				steer(-0.715)
-		# a = average(a,0)
-		# b = average(a,1)
+	def watch():
+	
+		self.watchlistL.append(distance(0))
+		if len(self.watchlistL) > measrange:
+			self.watchlistL = self.watchlistL[1:]
+			
+		b = distance(1)
+		self.watchlistR.append(b)
+		if len(self.watchlistR) > measrange:
+			self.watchlistR = self.watchlistR[1:]
+					
+	def obstancle():
 		
-		a = distance(0);
-		b = distance(1);
+		watch()
 		
-		stop()
+		while(len(watchlistL) < measrange and len(watchlistR) < measrange):
+			watch()
+			
+		if (watchlistL[len(watchlistL)-1] > deaththreshold and watchlistR[len(watchlistR)-1] > deaththreshold):
+			
+			(L,R) = alarm()
+			
+			if (L < R)
+				steer(-alarmL*movefactor)
+			if (R < L)
+				steer(alarmR*movefactor)
+		else:
+			drive(-2)
+			time.sleep(0.5)
+			stop()
+				
+			
+			
+
 		
-		steer(0.0)
+	
+	
