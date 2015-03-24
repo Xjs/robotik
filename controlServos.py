5a2c6d68834073612837a91d666d895beb5a2d49
glevF <feher@stud.uni-heidelberg.de>
2015-03-24 14:31:08 +0100

    Update

--- controlServos.py
+++ controlServos.py
@@ -1,6 +1,7 @@
 from RPIO import PWM
 
-servo = PWM.Servo()
+servo1 = PWM.Servo()
+servo2 = PWM.Servo()
 
 BASE = 1500
 RIGHT = (1800-1500) 
@@ -12,22 +13,22 @@ BACK = (1000-1500)
 
 def test():
 	# Set servo on GPIO17 (BCM) to (1.2ms)
-	servo.set_servo(17, 1600)
+	servo1.set_servo(17, 1600)
 
 	# Set servo on GPIO17 (BCM) (2.0ms)
-	servo.set_servo(17, 1200)
+	servo1.set_servo(17, 1200)
 
 	# Clear servo on GPIO17
-	servo.stop_servo(22)
+	servo1.stop_servo(17)
 	
 	# Set servo on GPIO22 (BCM) to (1.2ms)
-	servo.set_servo(22, 1600)
+	servo2.set_servo(22, 1600)
 
 	# Set servo on GPIO22 (BCM) (2.0ms)
 	#servo.set_servo(17, 1200)
 
 	# Clear servo on GPIO22
-	servo.stop_servo(22)
+	servo2.stop_servo(22)
 
 if __name__ == '__main__':
 	test()
