d78f9aee0dd43deb78d9625ce599066667a312f2
glevF <feher@stud.uni-heidelberg.de>
2015-03-24 14:18:48 +0100

    Update

--- controlServos.py
+++ controlServos.py
@@ -9,11 +9,16 @@ LEFT = (1170-1500)
 FORWARD = (1952-1500)
 BACK = (1000-1500)
 
-# Set servo on GPIO17 (BCM) to 1200µs (1.2ms)
-servo.set_servo(17, 1600)
 
-# Set servo on GPIO17 to 2000µs (2.0ms)
-servo.set_servo(17, 2000)
+def test():
+	# Set servo on GPIO17 (BCM) to (1.2ms)
+	servo.set_servo(17, 1600)
 
-# Clear servo on GPIO17
-servo.stop_servo(17)
+	# Set servo on GPIO17 (BCM) (2.0ms)
+	servo.set_servo(17, 1200)
+
+	# Clear servo on GPIO17
+	servo.stop_servo(17)
+
+if __name__ == '__main__':
+	test()
