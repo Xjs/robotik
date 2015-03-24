46933de298be6e82feabbbd6a07d9c66250840cb
glevF <feher@stud.uni-heidelberg.de>
2015-03-24 14:32:24 +0100

    Update

--- controlServos.py
+++ controlServos.py
@@ -1,4 +1,5 @@
 from RPIO import PWM
+import time
 
 servo1 = PWM.Servo()
 servo2 = PWM.Servo()
@@ -14,15 +15,21 @@ BACK = (1000-1500)
 def test():
 	# Set servo on GPIO17 (BCM) to (1.2ms)
 	servo1.set_servo(17, 1600)
+	
+	time.sleep(2)
 
 	# Set servo on GPIO17 (BCM) (2.0ms)
 	servo1.set_servo(17, 1200)
+
+	time.sleep(2)
 
 	# Clear servo on GPIO17
 	servo1.stop_servo(17)
 	
 	# Set servo on GPIO22 (BCM) to (1.2ms)
 	servo2.set_servo(22, 1600)
+
+	time.sleep(2)
 
 	# Set servo on GPIO22 (BCM) (2.0ms)
 	#servo.set_servo(17, 1200)
