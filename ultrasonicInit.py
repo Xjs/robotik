6f2788e634ff8cce1be3e624b993dd7e89a97d80
glevF <feher@stud.uni-heidelberg.de>
2015-04-12 16:42:48 +0200

    Initial commit

---
+++ ultrasonicInit.py
@@ -0,0 +1,21 @@
+from getDistance import *
+
+# This is an initialisation skript for running before starting the car.
+# Due to technical properties of the ultrasonic sensors, they seem to need 
+# some working time to run accurately.
+
+def init():
+	#Check left sensor
+	left = distance(0)
+	print("left: ", left)
+	
+	#Check right sensor
+	right = distance(1)
+	print("right: ", right)
+	
+if __name__ == "__main__":
+	for i in range(500):
+		init()
+		sleep(0.02) #wait 20 ms because sensors can only make one measurement every 20 ms
+	
+	#this takes 10 s  
